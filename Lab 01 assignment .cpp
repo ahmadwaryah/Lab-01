855630f94626dd8cac294f68e576f8f3f1e56334
ahmadwaryah <144982893+ahmadwaryah@users.noreply.github.com>
2023-10-12 15:10:37 +0500

    Add files via upload

---
+++ Lab 01 assignment .cpp
@@ -0,0 +1,64 @@
+//Lab 01 Home Task
+#include <iostream>
+using namespace std;
+
+// Task 01:
+// To get distance using formula.
+// int main() {
+//     float x, y, z, c, s,f ,o ,p ,i;
+//     cout<<"Coordinates?  "<<endl;
+//     cout<<"X1=";
+//     cin>>x;
+//     cout<<"X2=";
+//     cin>>y;
+//     cout<<"Y1=";
+//     cin>>z;
+//     cout<<"Y2=";
+//     cin>>c;
+//     s= (y-x);
+//     f= (c-z);
+//     o= s*s;
+//     p=f*f;
+//     i= o+p;
+//     cout<<"d="<<i;
+//     return 0;
+// }
+// Task 2:
+// To convert centimeters to meters and kilometers.
+// int main ()
+// {
+//     float x, z, y;
+//     cout<<"Length in Centimeters?  ";
+//     cin>>x;
+//     y=x/100;
+//     z=x/100000;
+//     cout<<"Distance in meters= "<<y<<endl;
+//     cout<<"Distance in Kilometers= "<<z<<endl;
+//     return 0;
+
+// }
+// Task 3:
+// To get the result of polynomial.
+// int main ()
+// {
+//     float x, y,z;
+//     cout<<"a=";
+//     cin>>x;
+//     cout<<"b=";
+//     cin>>y;
+//     z= (x*x)+(2*x*y)+(y*y);
+//     cout<<"Result of polynomial = "<<z<<endl;
+
+// }
+//Task 4:
+// To convert Fahrenheit to Celsius.
+// int main ()
+// {
+//     float x, y, z;
+//     cout<<"Temperature in Farenheit: ";
+//     cin>>x;
+//     y=(0.55)*(x-32);
+
+//     cout<<"Temperature in Celsius="<<y<<endl;
+//     return 0;
+// }
